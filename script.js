1bd366d71ab94af6fafb15ef62a6ad0611f1524a
Jacopo Pangrazi <jacopopangrazi@gmail.com>
2019-10-10 01:26:09 +0200

    Made popup and nav bar responsive

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// popup
+// Popup
 
 function togglePopup() {
   var popup = document.getElementById("popup");
@@ -7,5 +7,17 @@ function togglePopup() {
     popup.classList.remove("show");
   } else {
     popup.classList.add("show");
+  }
+}
+
+// Mobile menu
+
+function toggleMenu() {
+  var menu = document.getElementById("mob-nav");
+
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show");
+  } else {
+    menu.classList.add("show");
   }
 }
